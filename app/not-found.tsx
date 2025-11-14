822cf5fc5bdf3f849268f0f6d79ed6ef8f0dc9e5
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2025-11-14 15:09:34 +0530

    feat: added not-found page

---
+++ app/not-found.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { APP_NAME } from "@/lib/constants";
+import Image from "next/image";
+
+const NotFoundPage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <Image
+        src="https://res.cloudinary.com/dckzwvjj1/image/upload/v1763109382/logo_ztp5zf.svg"
+        height={48}
+        width={48}
+        alt={`${APP_NAME} logo`}
+        priority={true}
+      />
+      <div className="p-6 w-1/3 rounded-lg shadow-md text-center">
+        <h1 className="text-3xl font-bold mb-4">Page Not Found</h1>
+        <div className="p text-destructive">Page requested does not exist </div>
+        <Button
+          variant="outline"
+          className="mt-4 ml-2"
+          onClick={() => (window.location.href = "/")}
+        >
+          Back To Home
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default NotFoundPage;
