568eeabcb5efcf2af8ff21b34381cdb846dc67ef
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2025-11-16 02:07:36 +0530

    config: serverless environment

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,17 +1,14 @@
 "use server";
 
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "@/db/prisma";
 import { convertToPlainObject } from "../utils";
 import { LATEST_PRODUCT_LIMIT } from "../constants";
 
 // Get latest products
 export async function getLatestProducts() {
-  const prisma = new PrismaClient();
-
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCT_LIMIT,
     orderBy: { createdAt: "desc" },
   });
-
   return convertToPlainObject(data);
 }
