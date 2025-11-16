a679bdcf349602c4f7ccbb33f01899f0262565ac
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2025-11-17 02:30:33 +0530

    fix: better signup error handling

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -5,7 +5,7 @@ import { signInFormSchema, signUpFormSchema } from "../validators";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
 import { hashSync } from "bcrypt-ts-edge";
 import { prisma } from "@/db/prisma";
-// import { formatError } from "zod";
+import { formatError } from "../utils";
 
 export async function signInWithCredentials(
   prevState: unknown,
@@ -65,6 +65,6 @@ export async function signUpUser(prevState: unknown, formData: FormData) {
     if (isRedirectError(error)) {
       throw error;
     }
-    return { success: false, message: "User was not registered" };
+    return { success: false, message: formatError(error) };
   }
 }
